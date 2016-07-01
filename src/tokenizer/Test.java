a232e546d033eb3ef3ab8dcce642c29c1f010d9d
shahsaurabh0605 <shahsaurabh0103@gmail.com>
2016-07-02 00:27:15 +0530

    Add: Cosine function

--- src/tokenizer/Test.java
+++ src/tokenizer/Test.java
@@ -110,6 +110,18 @@ public class Test
      }
 
    }
+
+   public static double cosineSimilarity(double[] vectorA, double[] vectorB) {
+     double dotProduct = 0.0;
+     double normA = 0.0;
+     double normB = 0.0;
+     for (int i = 0; i < vectorA.length; i++) {
+        dotProduct += vectorA[i] * vectorB[i];
+        normA += Math.pow(vectorA[i], 2);
+        normB += Math.pow(vectorB[i], 2);
+     }   
+     return dotProduct / (Math.sqrt(normA) * Math.sqrt(normB));
+   }
     public static String removeComments(String code) {
   	   StringBuilder newCode = new StringBuilder();
   	   try (StringReader sr = new StringReader(code)) {
